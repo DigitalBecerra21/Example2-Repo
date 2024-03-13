b380451a6671af309c08e881f596010cc357f463
DigitalBecerra <ingenierobecerrabarcelo@gmail.com>
2024-03-13 16:33:22 -0600

    feat: Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.astro'],
+  theme: {
+    extend: {
+      colors:
+      {
+        PrimaryYellow: "#DBF227",
+        PrimaryBlue: "0000f"
+      }
+    },
+  },
+  plugins: [],
+}
+
